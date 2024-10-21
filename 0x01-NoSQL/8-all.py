ad54f53c5c4e7a749f803ab4456edb7671fa8d28
Yermehyaw <yermehyaw@gmail.com>
2024-10-21 15:37:31 -0700

    Allow import

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,13 +3,11 @@
 Modules Imported: pymongo
 
 """
-if __name__ == '__main__':
-    import pymongo
+import pymongo
 
 
-    def list_all(mongo_collection):
-        """Makes a list of a mobgodb collection"""
-        doc_list = [doc for doc in mongo_collection.find()]
+def list_all(mongo_collection):
+    """Makes a list of a mobgodb collection"""
+    doc_list = [doc for doc in mongo_collection.find()]
 
-        return doc_list
-    
+    return doc_list
