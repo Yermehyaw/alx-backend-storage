2646794d69f87b4d8f75ec160881a1e9ce8acec5
Yermehyaw <yermehyaw@gmail.com>
2024-10-23 08:17:17 +0100

    Use aggregation syntax

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 """
+Sorts the docs in a collection
+
+Modules Imported: None
+
+"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score"""
+    pipeline = [
+            {'$unwind': '$topics'},
+            {
+                '$addFields': {
+                    'averageScore': {'$multiply': ['$topics.score']}
+                }
+            }
+    ]
+
