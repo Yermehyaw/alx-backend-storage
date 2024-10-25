c844594d0cb23d69dd63304041bfc172bb13c498
Yermehyaw <yermehyaw@gmail.com>
2024-10-25 23:37:26 +0000

    Fix get mthd

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,6 +13,7 @@ import redis
 import uuid
 from typing import Any
 from typing import Callable
+from typing import Optional
 from typing import Union
 from typing import Tuple
 from typing import TypeVar
@@ -23,7 +24,7 @@ C = TypeVar('C', bound='Cache')
 R = TypeVar('R')
 
 
-def count_calls(method: Callable[[Union[str, bytes, int, float]], str]) -> Callable[[Any], str]:
+def count_calls(method: Callable) -> Callable:
     """Decorator to count the number of times a method is called"""
 
     @wraps(method)
@@ -33,9 +34,10 @@ def count_calls(method: Callable[[Union[str, bytes, int, float]], str]) -> Calla
         # create a count key with __qualname__ with an initial value of 0
         client.incr(method.__qualname__)
 
-        return method(self, *args, **kwargs)  # call and return from the method to be wrapped
+        return method(self, *args, **kwargs)  # execute methd and ret val
 
     return wrapper
+
 
 def call_history(method: Callable[[C, Any], R]) -> Callable[[C, Any], R]:
     """Decorates a method to store the history of its inputs and outputs"""
@@ -45,10 +47,10 @@ def call_history(method: Callable[[C, Any], R]) -> Callable[[C, Any], R]:
         """Wrapper to implemnt the decorator logic"""
         client = self._redis
 
-        return method(self, *args, **kwargs)  # return the expected return val of the decorated mthd
+        return method(self, *args, **kwargs)  # return val of decorated mthd
 
         # Create a redis db list of inputs to the method
-        client.rpush(method.__qualname__ + ':inputs', str(*args))  # arg is a list 
+        client.rpush(method.__qualname__ + ':inputs', str(*args))
 
         # Create a redisdb list of output from method
         client.rpush(method.__qualname__ + ':outputs', method_result)
@@ -59,15 +61,15 @@ def call_history(method: Callable[[C, Any], R]) -> Callable[[C, Any], R]:
 def replay(method: Callable[[C, Any], R]) -> None:
     """Displays the history of calls of a particular function"""
 
-    instance = method.__self__  # access the instance which the method was called from
+    instance = method.__self__  # access the method's instance
     client = instance._redis
-    
-    input_history = list(client.lrange(method.__qualname__ + ':inputs', 0, -1))  # retrieve the entire list from 0 to -1(end idx)
-    output_history = list(client.lrange(method.__qualname__ + ':outputs'))  # equiv to previous lrange() call
+
+    input_history = list(client.lrange(method.__qualname__ + ':inputs', 0, -1))
+    output_history = list(client.lrange(method.__qualname__ + ':outputs'))
     no_calls = int(client.get(method.__qualname__))
 
     # Display format
-    print(f'Cache.store was called {no_calls} times')  # decorator is for store() mthd
+    print(f'Cache.store was called {no_calls} times')
     for arg, ret in zip(input_history, output_history):
         print("Cache.store(*('{arg}',)) -> {ret}")
 
@@ -95,39 +97,23 @@ class Cache():
 
         return store_id
 
-    def get(self, key: str, fn: Callable[[], None]=None) -> Union[bytes, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         """
         Custom deserialiization of response
         value from a redis client
         """
-        if not fn or isinstance(fn, str):
-            return None
-
-        byte_key = bytes(key.decode('utf-8'))
-        print(byte_key)
-        if self._redis.type(key) == byte_key:
-            # if key is in bytes, return in bytes
-            return bytes(self._redis.get(key))
-
-        # Check if key exists
         value = self._redis.get(key)
 
-        if value:  # key exists
+        if value and fn:
             # custom behaviour
             return fn(value)  # decoded by callback func
 
-        return None  # default behaviour if key is not in db
+        return value  # defaut behaviour if fn isnt passed
 
-    def get_str(self, key: str) -> str:
+    def get_str(self, value: bytes) -> str:
         """Decode responses for str values"""
-        if self._redis.type(key) == 'bytes':
-            return self._redis.get(key)
+        return value.decode('utf-8')
 
-        return self._redis.get(key).decode('utf-8')
-
-    def get_int(self, key:str) -> int:
+    def get_int(self, value: bytes) -> int:
         """Decode responses for int values"""
-        if self._redis.type(key) == 'bytes':
-            return self._redis.get(key)
-
-        return self._redis.get(key).decode('utf-8')        
+        return value.decode('utf-8')
